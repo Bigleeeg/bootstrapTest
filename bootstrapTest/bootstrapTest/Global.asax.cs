e30d94c0856ff74096c79236638b1ad063ffc0a0
JIAOFENG <vip.f@live.cn>
2016-10-12 10:00:02 +0800

    submit

--- bootstrapTest/bootstrapTest/Global.asax.cs
+++ bootstrapTest/bootstrapTest/Global.asax.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
 namespace bootstrapTest
 {
-    public class Global : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
-        protected void Application_Start(object sender, EventArgs e)
+        void Application_Start(object sender, EventArgs e)
         {
+            // 在应用程序启动时运行的代码
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
